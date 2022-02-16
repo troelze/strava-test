4f392a94a88f18a7c4a30eb60f5de41ec8ce8c2f
Tres Oelze <toelze@gmail.com>
2022-02-15 21:48:49 -0600

    Add list of stats

--- src/pages/YourDistance.js
+++ src/pages/YourDistance.js
@@ -1,45 +1,40 @@
 import React from "react";
 import { connect } from "react-redux";
 import ListGroup from "react-bootstrap/ListGroup";
+import { Carousel } from "bootstrap";
 
 const YourDistance = ({ user, returnTokens }) => {
-  //console.log(Object.entries(user.data));
+  let arr = Object.entries(user.data);
+  var activityMap = {};
 
-  // sets table from Strava JSON Data
-  const userDataList = [
-    user.data.all_run_totals.distance,
-    user.data.all_ride_totals.distance,
-    user.data.all_swim_totals.distance,
-  ];
+  for (let i = 0; i < arr.length; i++) {
+    let distance = (arr[i][1].distance * 0.000621371).toFixed(2);
+    let activityName = arr[i][0];
+    activityMap[activityName] = distance;
+  }
 
-  // const sportList = ["run", "rode", "swam"];
-
-  // Creates Key-Object Mapping for React Component + converts Meters to Miles
-  const distanceObject = userDataList.map((distance, i) => ({
-    id: i,
-    title: (distance * 0.000621371).toFixed(2),
-  }));
-
-  //Validators
-  //console.log(userDataList);
-  //console.log(distanceObject);
-
+console.log(activityMap)
   return (
-    // Prints ListGroup to page mapping over Object w/ key
     <>
       <h1>
         Hi, {returnTokens.athlete.firstname} - here are your totals for the
-        year:{" "}
+        year:
       </h1>
-      <div className="card align-items-center" style={{ textAlign: `center` }}>
+      <div>
         <ListGroup>
-          {distanceObject.map((distances) => (
-            <ListGroup.Item key={distances.id}>
-              {distances.title} miles you've
-            </ListGroup.Item>
-          ))}
+                  <ListGroup.Item>all_ride_totals: { activityMap['all_ride_totals']}'] </ListGroup.Item>
+        <ListGroup.Item>all_run_totals:  { activityMap['all_run_totals']}</ListGroup.Item>
+        <ListGroup.Item>all_swim_totals: { activityMap['all_swim_totals']}</ListGroup.Item>
+        <ListGroup.Item>biggest_climb_elevation_gain: { activityMap['biggest_climb_elevation_gain']} </ListGroup.Item>
+        <ListGroup.Item>biggest_ride_distance:  { activityMap['biggest_ride_distance']}</ListGroup.Item>
+        <ListGroup.Item>recent_ride_totals: { activityMap['recent_ride_totals']} </ListGroup.Item>
+        <ListGroup.Item>recent_run_totals:{ activityMap['recent_run_totals']} </ListGroup.Item>
+        <ListGroup.Item>recent_swim_totals:  { activityMap['recent_swim_totals']}</ListGroup.Item>
+        <ListGroup.Item>ytd_ride_totals:{ activityMap['ytd_ride_totals']} </ListGroup.Item>
+        <ListGroup.Item>ytd_run_totals:  { activityMap['ytd_run_totals']}</ListGroup.Item>
+        <ListGroup.Item>ytd_swim_totals: { activityMap['ytd_swim_totals']} </ListGroup.Item>
         </ListGroup>
-      </div>
+    </div>
     </>
   );
 };
