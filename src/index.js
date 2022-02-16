690eb8f4322d401fa8e835f95a98ea64d2f00fc3
elimchayseng <elimchayseng13@gmail.com>
2022-02-15 22:58:12 -0600

    Add Dark Mode

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import { MantineProvider } from "@mantine/core";
 import AppRouter from "./router/AppRouter";
 import reducers from "./reducers";
 
@@ -15,7 +15,9 @@ ReactDOM.render(
         window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
   >
-    <AppRouter />
+    <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles>
+      <AppRouter />
+    </MantineProvider>
   </Provider>,
   document.getElementById("root")
 );
