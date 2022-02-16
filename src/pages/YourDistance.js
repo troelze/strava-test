82ff8e9481994d69e7e1935fccdc6382dadee426
Tres Oelze <toelze@gmail.com>
2022-02-15 22:48:40 -0600

    Dynamically add activity list

--- src/pages/YourDistance.js
+++ src/pages/YourDistance.js
@@ -5,16 +5,17 @@ import { Carousel } from "bootstrap";
 
 const YourDistance = ({ user, returnTokens }) => {
   let arr = Object.entries(user.data);
-    var activityMap = {};
-    
+  var activities = [];
 
   for (let i = 0; i < arr.length; i++) {
     let distance = (arr[i][1].distance * 0.000621371).toFixed(2);
     let activityName = arr[i][0];
-    activityMap[activityName] = distance;
+    let item = {
+      name: activityName,
+      distance: distance,
+    };
+    activities.push(item);
   }
-    
-    console.log(activityMap)
 
   return (
     <>
@@ -24,19 +25,13 @@ const YourDistance = ({ user, returnTokens }) => {
       </h1>
       <div>
         <ListGroup>
-                  <ListGroup.Item>all_ride_totals: { activityMap['all_ride_totals']}'] </ListGroup.Item>
-        <ListGroup.Item>all_run_totals:  { activityMap['all_run_totals']}</ListGroup.Item>
-        <ListGroup.Item>all_swim_totals: { activityMap['all_swim_totals']}</ListGroup.Item>
-        <ListGroup.Item>biggest_climb_elevation_gain: { activityMap['biggest_climb_elevation_gain']} </ListGroup.Item>
-        <ListGroup.Item>biggest_ride_distance:  { activityMap['biggest_ride_distance']}</ListGroup.Item>
-        <ListGroup.Item>recent_ride_totals: { activityMap['recent_ride_totals']} </ListGroup.Item>
-        <ListGroup.Item>recent_run_totals:{ activityMap['recent_run_totals']} </ListGroup.Item>
-        <ListGroup.Item>recent_swim_totals:  { activityMap['recent_swim_totals']}</ListGroup.Item>
-        <ListGroup.Item>ytd_ride_totals:{ activityMap['ytd_ride_totals']} </ListGroup.Item>
-        <ListGroup.Item>ytd_run_totals:  { activityMap['ytd_run_totals']}</ListGroup.Item>
-        <ListGroup.Item>ytd_swim_totals: { activityMap['ytd_swim_totals']} </ListGroup.Item>
+          {activities.map((item) => (
+            <ListGroup.Item>
+              {item.name}: {item.distance}
+            </ListGroup.Item>
+          ))}
         </ListGroup>
-    </div>
+      </div>
     </>
   );
 };
