ba45df9cc8c78d0a21b03d4b122b093a2ff6a074
Tres Oelze <toelze@gmail.com>
2022-04-07 17:34:04 -0500

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
+import cors from 'cors';
 import routes from './routes/athleteRoutes';
 
 require('dotenv').config();
@@ -18,6 +19,9 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL, {
 // bodyparser setup
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json());
+
+// CORS setup
+app.use(cors())
 
 routes(app);
 
